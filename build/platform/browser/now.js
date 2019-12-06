9657e48d8516b3ed3fb55fc8c5f624800e3b9801
anthony-tsang <anthony.tsang@samsara.com>
2019-12-05 18:52:01 -0800

    browser now: Test setting nowMilli.

--- build/platform/browser/now.js
+++ build/platform/browser/now.js
@@ -4,15 +4,21 @@
 // implementation is that we measure our performance in milliseconds rather than microseconds.
 // We are currently evaluating lightstep and want to be consistent with how we are gathering our duration metrics.
 // source: https://github.com/lightstep/lightstep-tracer-javascript/blob/c642dd59b4ed13e8832c27345b382f5dbd2a35eb/src/imp/platform/browser/platform_browser.js#L8
-module.exports = function () {
+let nowMilli = function () {
   // Is a hi-res timer available?
   if (window.performance &&
       window.performance.now &&
       window.performance.timing &&
       window.performance.timing.navigationStart) {
       let start = performance.timing.navigationStart;
-      return Math.floor((start + performance.now()));
+      return function () {
+        return Math.floor((start + performance.now()))
+      };
   }
   // The low-res timer is the best we can do
-  return Date.now();
-};
+  return function () {
+    return Date.now();
+  };
+}();
+
+module.exports = nowMilli;
